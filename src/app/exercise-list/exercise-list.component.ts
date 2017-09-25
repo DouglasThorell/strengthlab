bc0391586e5994a95e5d82550753187b97192404
Jakob Sanne <jakob.sanne@gmail.com>
2017-09-25 15:58:52 +0200

    temp remove sass

--- src/app/exercise-list/exercise-list.component.ts
+++ src/app/exercise-list/exercise-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-exercise-list',
   templateUrl: './exercise-list.component.html',
-  styleUrls: ['./exercise-list.component.scss']
+  styleUrls: ['./exercise-list.component.css']
 })
 export class ExerciseListComponent implements OnInit {
 
