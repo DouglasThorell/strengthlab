0ab403a4c3116d1cb22f2f5d49e293d1b3cb41ca
Douglas Thorell <douglas.thorell@gmail.com>
2017-09-25 14:09:23 +0200

    Firebase config, authentication issues

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
-import { AuthService} from '../shared/auth.service';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import * as firebase from 'firebase/app';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../shared/auth.service';
+import {Router, RouterModule} from '@angular/router';
+
 
 
 @Component({
@@ -10,19 +9,17 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  user = null;
-  topics: FirebaseListObservable<any[]>;
-  constructor(
-    private auth: AuthService,
-    public db: AngularFireDatabase) { }
-  ngOnInit() {
-    this.auth.getAuthState().subscribe(
-      (user) => this.user = user);
-    this.topics = this.db.list('/topics');
-  }
-  loginWithGoogle() {
-    this.auth.loginWithGoogle();
-  }
 
+export class LoginComponent implements OnInit {
+
+  constructor(public authService: AuthService, private router: Router) { }
+
+  login() {
+    this.authService.loginWithGoogle().then((data) => {
+      this.router.navigate(['/home']);
+    });
+
+  }
+  ngOnInit() {
+    }
 }
