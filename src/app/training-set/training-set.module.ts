c8bccba6890fd8e588307e434b89fc7cc168619a
Jakob Sanne <jakob.sanne@gmail.com>
2017-10-03 21:22:20 +0200

    typo fix

--- src/app/training-set/training-set.module.ts
+++ src/app/training-set/training-set.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { TrainingSetListComponent } from './training-set-list/training-set-list.component';
 import { TrainingSetDetailComponent } from './training-set-detail/training-set-detail.component';
 import { TrainingSetFormComponent } from './training-set-form/training-set-form.component';
-import { TrainingSet } from './training-set';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TrainingSet, TrainingSetListComponent, TrainingSetDetailComponent, TrainingSetFormComponent]
+  declarations: [TrainingSetListComponent, TrainingSetDetailComponent, TrainingSetFormComponent]
 })
 export class TrainingSetModule { }
