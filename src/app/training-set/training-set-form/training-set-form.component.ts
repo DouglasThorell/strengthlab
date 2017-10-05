268b41aaaca4ec9930b40c3ff751a293f6597163
Jakob Sanne <jakob.sanne@gmail.com>
2017-10-05 10:28:35 +0200

    sending data to set-compnt

--- src/app/training-set/training-set-form/training-set-form.component.ts
+++ src/app/training-set/training-set-form/training-set-form.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {TrainingSetService} from '../training-set.service';
 import {TrainingSet} from '../training-set';
+import {Exercise} from '../../exercise/shared/exercise';
 
 @Component({
   selector: 'app-training-set-form',
   templateUrl: './training-set-form.component.html',
   styleUrls: ['./training-set-form.component.scss']
 })
-export class TrainingSetFormComponent implements OnInit {
+export class TrainingSetFormComponent implements OnInit { // @Input() exercise: Exercise;
   trainingSet = new TrainingSet;
   constructor(private trainingSetService: TrainingSetService) { }
 
--- src/app/training-set/training-set.module.ts
+++ src/app/training-set/training-set.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { TrainingSetListComponent } from './training-set-list/training-set-list.component';
 import { TrainingSetDetailComponent } from './training-set-detail/training-set-detail.component';
 import { TrainingSetFormComponent } from './training-set-form/training-set-form.component';
-import {MaterialModule} from "../shared/modules/material.module";
-import {SharedModule} from "../shared/modules/shared.module";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {TrainingSetService} from "./training-set.service";
+import {SharedModule} from '../shared/modules/shared.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TrainingSetService} from './training-set.service';
 
 @NgModule({
   imports: [
