0a350a4360ec786536bd4596f5abfc1a6f7e0410
Jakob Sanne <jakob.sanne@gmail.com>
2017-10-09 09:36:22 +0200

    router parameters

--- src/app/exercise/current-exercise/current-exercise.component.ts
+++ src/app/exercise/current-exercise/current-exercise.component.ts
@@ -1,20 +1,29 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Exercise} from "../shared/exercise";
-import {ActivatedRoute} from "@angular/router";
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Exercise} from '../shared/exercise';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-current-exercise',
   templateUrl: './current-exercise.component.html',
   styleUrls: ['./current-exercise.component.scss']
 })
-export class CurrentExerciseComponent implements OnInit {
+export class CurrentExerciseComponent implements OnInit, OnDestroy {
+
   @Input() exercise: Exercise;
+
   exerciseName: String;
+  private sub: any;
+
   constructor(
     private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {this.exerciseName = params['exerciseName']})
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
